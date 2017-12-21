bec231294ef8600e86233ecee043acb6789b00c1
Shankar Kulumani <shanks.k@gmail.com>
2017-12-20 20:16:44 -0500

    Automatically add the compiled thesis to a tagged release
    Create a tag if none exist

---
+++ utilities/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# deploy to github tagged releases
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "Tag $TRAVIS_TAG exists"
+else
+    echo "No current tag. Lets make one"
+    git config --local user.name='travis'
+    git config --local user.email='travis'
+    git tag "$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
+fi
