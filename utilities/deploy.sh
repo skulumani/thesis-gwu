4366233acad015b803810c37a700c97be51b2a49
Shankar Kulumani <shanks.k@gmail.com>
2017-12-20 20:19:37 -0500

    Fix deploy script
    Error in setting name for travis

--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -5,7 +5,7 @@ if [ -n "$TRAVIS_TAG" ]; then
     echo "Tag $TRAVIS_TAG exists"
 else
     echo "No current tag. Lets make one"
-    git config --local user.name='travis'
-    git config --local user.email='travis'
+    git config --local user.name 'travis'
+    git config --local user.email 'travis'
     git tag "$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)"
 fi
