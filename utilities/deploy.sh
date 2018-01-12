5b72a00ff05faa0577fb8ee214c4729e4574c682
Shankar Kulumani <shanks.k@gmail.com>
2018-01-12 08:32:49 -0500

    Don't auto make tags

--- utilities/deploy.sh
+++ utilities/deploy.sh
@@ -4,10 +4,10 @@
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Tag $TRAVIS_TAG exists"
 else
-    echo "No current tag. Lets make one"
-    git config --local user.name 'travis'
-    git config --local user.email 'travis'
-    git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag"
+    echo "No current tag. We're not going to make a tag!"
+    # git config --local user.name 'travis'
+    # git config --local user.email 'travis'
+    # git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag"
 
     echo "Done making a tag"
 fi
