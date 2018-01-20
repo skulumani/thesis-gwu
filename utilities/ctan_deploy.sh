255ca7f739f8b042c76f1cd9b817a865d7df557d
Shankar Kulumani <shanks.k@gmail.com>
2018-01-20 14:14:37 -0500

    Add a script for ctan deploy

---
+++ utilities/ctan_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir thesis-gwu
+cp -rt ./thesis-gwu figures tex LICENSE.md README.md thesis-bib.bib thesis-gwu.cls thesis-sample.pdf thesis-sample.tex
+
+zip -r9 thesis-gwu.zip  thesis-gwu 
+
+# Can use ctan-o-mat to automatticaly deploy
+# modify thesis-gwu.pkg version number and anything else
+# then run ctan-o-mat -v --<validate or submit> --pkg ./utilities/thesis-gwu.pkg
+ctan-o-mat -v --validate --pkg ./thesis-gwu.pkg
+
+ctan-o-mat -v --submit --pkg ./thesis-gwu.pkg
